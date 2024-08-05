d4cc5d7ae4d23034be8c1eb3cd2d47d9ab09ac29
Killua <elatarmohamed158@gmail.com>
2024-08-05 18:12:16 +0300

    Initial start point

---
+++ app/src/main/java/com/mobjoy/klivvrinternshiptask/klivvrapp/klivvrTaskApp.kt
@@ -0,0 +1,9 @@
+package com.mobjoy.klivvrinternshiptask.klivvrapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class klivvrTaskApp : Application() {
+
+}
